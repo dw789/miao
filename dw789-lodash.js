c08deae5b519394ce91d3d12cb50d2c72bab2802
dawei <1191327664@qq.com>
2019-01-10 11:58:19 +0800

    lodash

--- dw789-lodash.js
+++ dw789-lodash.js
@@ -1,5 +1,9 @@
 var dw789 = {
-    chunk: function chunk(array, []) {
-
-    }
+    chunk: function chunk(array, size) {
+        var result = []
+        for (var i = 0; i < array.length; i++) {
+            result.push(array.slice(i, i + size));
+        }
+        return result;
+    },
 }
